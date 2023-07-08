5e443d35572ef0ef998d1ba4947da595cf593787
sajanwrs <sajanwrs@gmail.com>
2023-07-07 23:07:57 -0400

    completed styling navbar

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Script for scrolling to the specified section
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+//Fix for navbar overlapping problem
+const navHeight = Math.floor(
+  document.querySelector(".container-nav").offsetHeight
+);
+
+document.documentElement.style.setProperty(
+  "--scroll-padding",
+  navHeight + "px"
+);
