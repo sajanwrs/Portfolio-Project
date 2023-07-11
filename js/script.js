55b76ae99a880af3bcec4be942f8e2e4e0a80dc5
sajanwrs <sajanwrs@gmail.com>
2023-07-10 22:44:56 -0400

    fixed height issue

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// window.addEventListener("scroll", function () {
+//   var navi = document.querySelector("nav");
+//   navi.classList.toggle("sticky", window.scrollY > 0);
+// });
+
 // Script for scrolling to the specified section
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
